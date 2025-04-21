7976db0a73b27b1e649c8f6820c5311e63c41608
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-04-21 13:20:27 +0200

    Update App.js

--- hausaufgabe/src/App.js
+++ hausaufgabe/src/App.js
@@ -6,8 +6,8 @@ function App() {
   const [colour, setColour] = useState("black")
 
   useEffect(() => {
-    console.log("Farbe wurde geändert")
-  }, [colour])
+    console.log("Farbe wurde geändert");
+  }, [colour]);
 
   function reset(){
     setZahl(0)
@@ -19,18 +19,18 @@ function App() {
 
   return (
    <>
-   <p>Meine Lieblingszahl ist {zahl}</p>
+   <p>Zahl ist {zahl}</p>
 
    <button onClick={() => setZahl(zahl + 1) }>+</button>
    <button onClick={() => setZahl(zahl - 1) }>-</button>
-   <button onClick={reset()}>Reset</button>
+   <button onClick={reset}>Reset</button>
 
    <h1 style={{color: colour}}>Ich bin eine Überschrift</h1>
    <button onClick={() => setColour("blue")}>Ändere die Farbe zu blau</button>
    <button onClick={() => setColour("green")}>Ändere die Farbe zu grün</button>
    <button onClick={() => setColour("orange")}>Ändere die Farbe zu orange</button>
    <button onClick={() => setColour("red")}>Ändere die Farbe zu rot</button>
-   <button onClick={resetColour()}>Ändere die Farbe zurück</button>
+   <button onClick={resetColour}>Ändere die Farbe zurück</button>
    </>
   );
 }
